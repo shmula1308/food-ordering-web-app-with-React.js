6d3f28ec147d1693607a9301e354f6530a75294a
Shpend Mula <shmula@hotmail.com>
2021-08-06 14:12:51 +0200

    added navigation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import { Fragment } from "react";
-import "./App.css";
+import { useEffect, useState } from "react";
 import Header from "./components/Header/Header";
+import classes from "./App.module.css";
+import { MdShoppingCart } from "react-icons/md";
+import { MdRestaurantMenu } from "react-icons/md";
 
-function App() {
+function App(props) {
+  const [opaque, setOpaque] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      setOpaque(window.pageYOffset > 200);
+    });
+  }, []);
+
   return (
-    <Fragment>
+    <div className={classes.app}>
+      <nav className={`${classes.nav} ${opaque ? classes.opaque : ""}`}>
+        <h1>Pizzeria Napoli</h1>
+        <div className={classes.actions}>
+          <button className={classes.button}>
+            <MdRestaurantMenu className={classes.icon} />
+          </button>
+          <button className={classes.button}>
+            <span className={`${classes.badge} ${classes.bounceIn}`}>13</span>
+            <MdShoppingCart className={classes.icon} />
+          </button>
+        </div>
+      </nav>
       <Header />
-    </Fragment>
+    </div>
   );
 }
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,1 +1,15 @@
+import React from "react";
+import classes from "./Header.module.css";
+import headerImage from "../../assets/plate.jpg";
 
+const Header = (props) => {
+  return (
+    <header>
+      <div className={classes.banner}>
+        <img src={headerImage} alt="Pizza's on a table" />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
